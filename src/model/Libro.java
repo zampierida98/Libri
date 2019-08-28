451cbaf1954669ca6d37179d26eeb68f35f2b211
Raffaele Berardo <raffaele.berardo812@gmail.com>
2019-08-28 12:39:35 +0200

    Tabella con tutti gli Ordini senza BoxComboModel

--- src/model/Libro.java
+++ src/model/Libro.java
@@ -99,5 +99,11 @@ public class Libro {
 	public void setPunti(int punti) {
 		this.punti = punti;
 	}
+	
+
+	@Override
+	public String toString() {
+		return "[ISBN=" + ISBN + " titolo=" + titolo + "\n]";
+	}
 
 }
--- src/view/ViewOrdine.java
+++ src/view/ViewOrdine.java
@@ -1,35 +1,49 @@
 package view;
 
+import java.awt.Component;
 import java.util.List;
 
 import javax.swing.BoxLayout;
-import javax.swing.JComboBox;
 import javax.swing.JPanel;
 import javax.swing.JTable;
+import javax.swing.table.TableCellRenderer;
+import javax.swing.table.TableColumnModel;
 
+import model.Libro;
 import model.Ordine;
 
 public class ViewOrdine extends JPanel{
-	
+
 	//campi dati
 	private List<Ordine> listaOrdini;
-	
-	private static Integer[] allID(List<Ordine> listaOrdini) {
+
+	//campi per GUI
+	//private static JComboBox boxOrdini;
+	private static final String[] columns = {"Data", "Lista Libri", "Costo Totale", "Pagamento", "Indirizzo spedizione", "Punti Accumulati"};
+	private static Object[][] data;
+	private static JTable tabellaOrdini;
+
+	/*private Integer[] allID(List<Ordine> listaOrdini) {
 		Integer[] allID;
 		allID = new Integer[listaOrdini.size()];
 		for(int i = 0; i < listaOrdini.size(); i++) {
 			allID[i] = listaOrdini.get(i).getIdOrdine();
 		}
 		return allID;
-	}
-	
-	private static Object[][] allOrders(List<Ordine> listaOrdini){
+	}*/
+
+	private Object[][] allOrders(List<Ordine> listaOrdini){
 		int i = 0;
 		Object[][] orders;
-		orders = new Object[listaOrdini.size()][];
+		orders = new Object[listaOrdini.size()][6];
 		for(Ordine ordine : listaOrdini){
 			orders[i][0] = ordine.getData();
-			orders[i][1] = ordine.getListaLibri();
+			//orders[i][1] = ordine.getListaLibri().toString();
+			String listaLibri = "<html>";
+			for(Libro libro : ordine.getListaLibri()) {
+				listaLibri += (libro.toString() + "<br/>");
+			}
+			orders[i][1] = listaLibri + "</html>";
 			orders[i][2] = ordine.getCostoTotale();
 			orders[i][3] = ordine.getPagamento();
 			orders[i][4] = ordine.getSpedizione();
@@ -38,60 +52,67 @@ public class ViewOrdine extends JPanel{
 		}
 		return orders;
 	}
-	
-	//metodi get/set
-	//EMAIL
-	/*public String getEmail() {
-		return email;
-	}
 
-	public void setEmail(String email) {
-		this.email = email;
-	}*/
-	
+	//metodi get/set
+	/*public JComboBox getBoxOrdini() {
+		return this.boxOrdini;
+	}
+	 */
+
 	public void setListaOrdini(List<Ordine> listaOrdini) {
 		this.listaOrdini = listaOrdini;
 	}
-	
-	
+
+
 	//INIZIO GUI
 	//contenitore card
-	/*private static final JPanel card = new JPanel(new CardLayout());
 
-	//bottoni cambia card ordine
-	private static final JButton visualizzaOrdini =  new JButton("Visualizza ordine");
-	private static final JButton inserisciOrdine = new JButton ("Inserisci ordine");*/
-	
-	//default card visualizza ordini
-	//card VISUALIZZA ORDINE
-	/*private final JComboBox boxOrdini = new JComboBox(allID(listaOrdini));
-	private String[] columns = {"Data", "Lista Libri", "Costo Totale", "Pagamento", "Indirizzo spedizione", "Punti Accumulati"};
-	private Object[][] data = ViewOrdine.allOrders(listaOrdini);
-	private final JTable tabellaOrdini = new JTable(data, columns);
-	*/
-	private static JComboBox boxOrdini;
-	private static final String[] columns = {"Data", "Lista Libri", "Costo Totale", "Pagamento", "Indirizzo spedizione", "Punti Accumulati"};
-	private static Object[][] data;
-	private static JTable tabellaOrdini ;
-	
+	public void resizeColumnWidth(JTable table) {
+		final TableColumnModel columnModel = table.getColumnModel();
+		for (int column = 0; column < table.getColumnCount(); column++) {
+			int width = 15; // Min width
+			for (int row = 0; row < table.getRowCount(); row++) {
+				TableCellRenderer renderer = table.getCellRenderer(row, column);
+				Component comp = table.prepareRenderer(renderer, row, column);
+				width = Math.max(comp.getPreferredSize().width +1 , width);
+			}
+			if(width > 300)
+				width=300;
+			columnModel.getColumn(column).setPreferredWidth(width);
+		}
+	}
+
+	private void AltezzaRighe() {
+		int riga = 0;
+		for(Ordine ordine : this.listaOrdini){
+			this.tabellaOrdini.setRowHeight(riga , ordine.getListaLibri().size()*16);
+			riga++;
+		}
+	}
+
+
 	public ViewOrdine(List<Ordine> listaOrdini) {
-		boxOrdini = new JComboBox(allID(listaOrdini));
-		data = ViewOrdine.allOrders(listaOrdini);
+		this.listaOrdini = listaOrdini;
+		//boxOrdini = new JComboBox(allID(listaOrdini));
+		data = this.allOrders(listaOrdini);
 		tabellaOrdini = new JTable(data, columns);
-		boxOrdini.setSelectedIndex(0);
+		this.AltezzaRighe();
+		resizeColumnWidth(tabellaOrdini);
+		tabellaOrdini.setAutoResizeMode(JTable.AUTO_RESIZE_LAST_COLUMN);
+		//boxOrdini.setSelectedIndex(0);
 		this.setLayout(new BoxLayout(this, BoxLayout.PAGE_AXIS));
-		this.add(boxOrdini);
+		//this.add(boxOrdini);
 		this.add(tabellaOrdini);
 	}
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
+
+
+
+
+
+
+
+
+
+
+
 }
