d3af3894742ffb4c9ccf41c36cbd9a2d3d1a1f48
Raffaele Berardo <raffaele.berardo812@gmail.com>
2019-09-01 17:00:40 +0200

    Modifica del Profilo
    Schermata di modifica profilo.
    Non funziona listener ModificaProfiloListener().
    Errore quando fai getText()

---
+++ src/view/ModificaProfilo.java
@@ -0,0 +1,67 @@
+package view;
+
+import java.awt.GridLayout;
+
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+import controller.ModificaProfiloListener;
+import model.Utente;
+
+public class ModificaProfilo extends JPanel {
+	
+	private String[] arrayLabel = {"Nome: ", "Cognome: ", "Indirizzo: ", "Telefono: ", "Password: "};
+	private String email;
+	private JTextField nome;
+	private JTextField cognome;
+	private JTextField indirizzo;
+	private JTextField telefono;
+	private JTextField password;
+	
+	private JButton modificaProfiloButton = new JButton("Modifica Dati");
+	
+	public String getEmail() {
+		return email;
+	}
+	
+	public JTextField getNome() {
+		return nome;
+	}
+
+	public JTextField getCognome() {
+		return cognome;
+	}
+
+	public JTextField getIndirizzo() {
+		return indirizzo;
+	}
+
+	public JTextField getTelefono() {
+		return telefono;
+	}
+
+	public JTextField getPassword() {
+		return password;
+	}
+	
+	public JButton getModificaProfiloButton() {
+		return modificaProfiloButton;
+	}
+
+	public ModificaProfilo(Utente u) {
+		this.setLayout(new GridLayout(7, 2));
+		this.email = u.getEmail();
+		String[] utente = {u.getNome(), u.getCognome(), u.getIndirizzo(), u.getTelefono(),  u.getPassword() };
+		JTextField[] modificaCampi = {nome, cognome, indirizzo, telefono, password};
+		for(int i = 0; i < arrayLabel.length; i++) {
+			JLabel campo = new JLabel(arrayLabel[i]);
+			this.add(campo);
+			modificaCampi[i] = new JTextField();
+			this.add(modificaCampi[i]);
+		}
+		this.add(modificaProfiloButton);
+		modificaProfiloButton.addActionListener(new ModificaProfiloListener(this));
+	}
+}
